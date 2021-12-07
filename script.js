1d9fca3e5a6cde0b6a1c228222bea3aaf138cfb8
RSTIEHM <92314719+RSTIEHM@users.noreply.github.com>
2021-12-06 20:03:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello!')
+const toggle = document.querySelector('.toggler');
+const body = document.querySelector('body');
+toggle.addEventListener('change', (e) => {
+  let val = e.target.value;
+  if (val == '0') {
+    body.classList.add('dark');
+  }
+  if (val == '100') {
+    body.classList.remove('dark');
+  }
+});
